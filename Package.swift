03d9483147e752117b2de80d1914b2b42b0e7c01
yeskay-zohocorp <kasiviswanath.sk@zohocorp.com>
2023-12-15 18:49:14 +0530

    Revert "Release 8.0.0"
    This reverts commit 8af91a771e9d87da44967577c06f364aa614e3ba.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "8.0.0"
+let version = "7.0.0"
 let package = Package(
     name: "Mobilisten",
     platforms: [.iOS(.v11)],
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mobilisten",
-            url: "https://raw.githubusercontent.com/zoho/SalesIQ-Mobilisten-iOS/v\(version)/Mobilisten.zip", checksum: "97e47ac5f396e13e0169d05fd0a4a1b1659c13f7a155fe4b167ba030f0d86511")
+            url: "https://raw.githubusercontent.com/zoho/SalesIQ-Mobilisten-iOS/v\(version)/Mobilisten.zip", checksum: "9cc23de74fb2fa3b8f14601f8516cd0ef62a61a27c7dcdd36821d9fd14aa09b3")
     ]
 )
