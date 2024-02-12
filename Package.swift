f619ce95f20a1d4145879a2af2c80e9dfc819668
valliammai-sv <valliammai.s@zohocorp.com>
2024-02-12 17:31:34 +0530

    Release 8.0.3

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "8.0.2"
+let version = "8.0.3"
 let package = Package(
     name: "Mobilisten",
     platforms: [.iOS(.v11)],
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mobilisten",
-            url: "https://raw.githubusercontent.com/zoho/SalesIQ-Mobilisten-iOS/v\(version)/Mobilisten.zip", checksum: "a1701f555bce1dec6f40b8828fad8485d1fdc73ae72108f26c709ef9361827a5")
+            url: "https://raw.githubusercontent.com/zoho/SalesIQ-Mobilisten-iOS/v\(version)/Mobilisten.zip", checksum: "04878fd224df65453eb4bff59bc04864f6ae19388640ee528466be85b519b4b1")
     ]
 )
