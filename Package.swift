2fbfc702c65309629db16d3db026c14fd31f490b
Kasiviswanath Senthilkumar <kasiviswanath.sk@zohocorp.com>
2025-09-23 18:05:49 +0530

    Release: Mobilisten - 10.1.3
    Release: Mobilisten - 10.1.3

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let mobilisten_version = "v10.1.2"
+let mobilisten_version = "v10.1.3"
 let mobilisten_calls_version = "Calls-1.0.1"
 let package = Package(
     name: "Mobilisten",
